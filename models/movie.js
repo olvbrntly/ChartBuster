82338b92bb58814c160dfc99439d54980b09ce02
olivia <olivia.brantley9@gmail.com>
2023-07-14 20:41:04 -0700

    added full name virtual

--- models/movie.js
+++ models/movie.js
@@ -4,26 +4,44 @@ const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
     title:{
-        type:String
+        type:String,
+        required:true
     },
     director_fname:{
-        type:String
+        type:String,
+        required:true
     },
     director_lname:{
-        type:String
+        type:String,
+        required:true
     },
     genre:[{
         type:Schema.Types.ObjectId,
         ref:"Genre"
     }],
     description:{
-        type:String
+        type:String,
+        required:true
     },
     run_time:{
         type:Number
+    },
+    year_released:{
+        type:Date,
+        required:true
     }
 
 
 })
+
+// directors full name
+MovieSchema.virtual("full_name").get(function(){
+    let fullname="";
+    if(this.director_fname && this.director_lname){
+        fullname = `${this.director_lname}, ${this.director_fname}`
+    }
+    return fullname
+})
+//
 
 module.exports = mongoose.model("Movie", MovieSchema)
