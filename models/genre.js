942edc27ab1c5a96cbe14afb41c2aa209d495475
olivia <olivia.brantley9@gmail.com>
2023-07-14 21:21:31 -0700

    changed title to name/dropped description

--- models/genre.js
+++ models/genre.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
 const GenreSchema = new Schema({
-    title:{
+    name:{
         type:String
     },
-    description:{
-        type:String
-    },
+   
 });
 
 module.exports = mongoose.model("Genre", GenreSchema)
