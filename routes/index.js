2e9649e0032796a112e46d3944adfb3d9011908d
olive <olivia.brantley9@gmail.com>
2023-07-14 17:01:17 -0700

    .env test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv');
+dotenv.config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(`port:${process.env.PORT}`)
+  res.render('index', { title: 'Express'});
+  
 });
 
 module.exports = router;
+
