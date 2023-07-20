dac8efc4d404e4d2ccd7789a10caee9e2f3cb147
olivia <olivia.brantley9@gmail.com>
2023-07-20 16:44:52 -0700

    outlined controllers

---
+++ controllers/copyController.js
@@ -0,0 +1,43 @@
+const AsyncHandler = require('express-async-handler');
+const Copy = require('../models/copy');
+
+
+// list out copys
+exports.copy_list = asyncHandler(async(req,res,next) =>{
+    res.send("not implemented copy_list")
+})
+
+//show details of copy
+exports.copy_detail = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy details")
+})
+
+//create form for getting copy info
+exports.copy_get_create = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy get create")
+})
+
+//create form for post copy info
+exports.copy_post_create = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy post create")
+})
+
+//update form for getting copy info
+exports.copy_get_update = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy get update")
+})
+
+//update form for getting copy info
+exports.copy_get_update = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy get update")
+})
+
+//delete form for getting copy info
+exports.copy_get_delete = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy get delete")
+})
+
+//delete form for getting copy info
+exports.copy_get_delete = asyncHandler(async(req,res,next) =>{
+    res.semd("not implemented: copy get delete")
+})
