ed29815fb9ad4e9092ecd3cfd514c343109cab7b
olivia <olivia.brantley9@gmail.com>
2023-07-20 16:44:44 -0700

    outlined controllers

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,15 +7,36 @@ exports.movie_list = asyncHandler(async(req,res,next) => {
 })
 
 //display detail page
+exports.movie_detail = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author Details');
+})
 
 //hadle create on get
+exports.movie_get_create = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author Get Create');
+})
 
 //handle create on post
+exports.movie_post = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author create POst');
+})
 
 //handle update on get
+exports.movie_list = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author get update');
+})
 
 //handle update on post
+exports.movie_list = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author post update');
+})
 
 //handle delete on get
+exports.movie_list = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author get delete');
+})
 
 //handle delete on post
+exports.movie_list = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author post delete');
+})
