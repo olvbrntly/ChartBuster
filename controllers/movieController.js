b7aebd54bb6cb7f3a7346f2eca2a776837a995d9
olivia <olivia.brantley9@gmail.com>
2023-07-20 11:41:50 -0700

    created contoller file and added outline

---
+++ controllers/movieController.js
@@ -0,0 +1,21 @@
+const Movie = require('../model/movie');
+const asyncHandler = require("express-async-handler");
+
+//Display list of Movies
+exports.movie_list = asyncHandler(async(req,res,next) => {
+    res.send('Not implemented:Author List');
+})
+
+//display detail page
+
+//hadle create on get
+
+//handle create on post
+
+//handle update on get
+
+//handle update on post
+
+//handle delete on get
+
+//handle delete on post
