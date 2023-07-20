938e99a67ba7d800b365044754f2eb28f4359b7c
olivia <olivia.brantley9@gmail.com>
2023-07-20 16:53:25 -0700

    fixed spelling error

--- controllers/copyController.js
+++ controllers/copyController.js
@@ -9,35 +9,35 @@ exports.copy_list = asyncHandler(async(req,res,next) =>{
 
 //show details of copy
 exports.copy_detail = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy details")
+    res.send("not implemented: copy details")
 })
 
 //create form for getting copy info
 exports.copy_get_create = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy get create")
+    res.send("not implemented: copy get create")
 })
 
 //create form for post copy info
 exports.copy_post_create = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy post create")
+    res.send("not implemented: copy post create")
 })
 
 //update form for getting copy info
 exports.copy_get_update = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy get update")
+    res.send("not implemented: copy get update")
 })
 
 //update form for getting copy info
 exports.copy_get_update = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy get update")
+    res.send("not implemented: copy get update")
 })
 
 //delete form for getting copy info
 exports.copy_get_delete = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy get delete")
+    res.send("not implemented: copy get delete")
 })
 
 //delete form for getting copy info
 exports.copy_get_delete = asyncHandler(async(req,res,next) =>{
-    res.semd("not implemented: copy get delete")
+    res.send("not implemented: copy get delete")
 })
