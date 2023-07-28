4127e9eb321a585da1cd5acfd10c58e0ade22c56
olivia <olivia.brantley9@gmail.com>
2023-07-28 10:10:31 -0700

    added comments

--- routes/route.js
+++ routes/route.js
@@ -8,22 +8,28 @@ const copyController = require("../controllers/copyController")
 
 // ----- MOVIE ROUTES -----
 
+//will display list of movies as default
 router.get("/", movieController.movie_list);
 
+//handles the creation of movies
 router.get("/movie/create", movieController.movie_get_create);
 
 router.post("/movie/create",movieController.movie_post_create);
 
+//handles deletion of movies
 router.get("/movie/delete", movieController.movie_get_delete);
 
 router.post("/movie/delete", movieController.movie_post_delete);
 
+//handles updating movies
 router.get("/movie/update", movieController.movie_get_update)
 
 router.post("/movie/update", movieController.movie_post_update)
 
+//displays the detail of the selected movie
 router.get("/movie:id", movieController.movie_detail)
 
+//displays list of movies
 router.get("/movie", movieController.movie_list)
 
 
