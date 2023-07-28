50d94fec62ce98e4cdb0e0774c57b4ebad4c5a61
olivia <olivia.brantley9@gmail.com>
2023-07-28 11:03:13 -0700

    edited send message

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,40 +3,40 @@ const asyncHandler = require("express-async-handler");
 
 //Display list of Movies
 exports.movie_list = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author List');
+    res.send('Not implemented:Movie List');
 })
 
 //display detail page
 exports.movie_detail = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author Details');
+    res.send('Not implemented:Movie Details');
 })
 
 //hadle create on get
 exports.movie_get_create = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author Get Create');
+    res.send('Not implemented:Movie Get Create');
 })
 
 //handle create on post
 exports.movie_post_create = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author create POst');
+    res.send('Not implemented:Movie create POst');
 })
 
 //handle update on get
 exports.movie_get_update = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author get update');
+    res.send('Not implemented:Movie get update');
 })
 
 //handle update on post
 exports.movie_post_update = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author post update');
+    res.send('Not implemented:Movie post update');
 })
 
 //handle delete on get
 exports.movie_get_delete = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author get delete');
+    res.send('Not implemented:Movie get delete');
 })
 
 //handle delete on post
 exports.movie_post_delete = asyncHandler(async(req,res,next) => {
-    res.send('Not implemented:Author post delete');
+    res.send('Not implemented:Movie post delete');
 })
