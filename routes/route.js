000089a5fdef523cdb52c9235bf467448cf6022a
olivia <olivia.brantley9@gmail.com>
2023-07-28 10:59:58 -0700

    fixed typos in routes

--- routes/route.js
+++ routes/route.js
@@ -9,7 +9,7 @@ const copyController = require("../controllers/copyController")
 // ----- MOVIE ROUTES -----
 
 //will display list of movies as default
-router.get("/", movieController.movie_list);
+router.get("", movieController.movie_list);
 
 //handles the creation of movies
 router.get("/movie/create", movieController.movie_get_create);
@@ -17,14 +17,14 @@ router.get("/movie/create", movieController.movie_get_create);
 router.post("/movie/create",movieController.movie_post_create);
 
 //handles deletion of movies
-router.get("/movie/delete", movieController.movie_get_delete);
+router.get("/movie/:id/delete", movieController.movie_get_delete);
 
-router.post("/movie/delete", movieController.movie_post_delete);
+router.post("/movie/:id/delete", movieController.movie_post_delete);
 
 //handles updating movies
-router.get("/movie/update", movieController.movie_get_update)
+router.get("/movie/:id/update", movieController.movie_get_update)
 
-router.post("/movie/update", movieController.movie_post_update)
+router.post("/movie/:id/update", movieController.movie_post_update)
 
 //displays the detail of the selected movie
 router.get("/movie:id", movieController.movie_detail)
@@ -40,17 +40,17 @@ router.get("/copy/create", copyController.copy_get_create);
 router.post("/copy/create",copyController.copy_post_create);
 
 //handles deletion of copies
-router.get("/copy/delete", copyController.copy_get_delete);
+router.get("/copy/:id/delete", copyController.copy_get_delete);
 
-router.post("/copy/delete", copyController.copy_post_delete);
+router.post("/copy/:id/delete", copyController.copy_post_delete);
 
 //handles updating copies
-router.get("/copy/update", copyController.copy_get_update)
+router.get("/copy/:id/update", copyController.copy_get_update)
 
-router.post("/copy/update", copyController.copy_post_update)
+router.post("/copy/:id/update", copyController.copy_post_update)
 
 //displays the detail of the selected copy
-router.get("/copy:id", copyController.copy_detail)
+router.get("/copy/:id", copyController.copy_detail)
 
 //displays list of copies
 router.get("/copy", copyController.copy_list)
@@ -63,17 +63,20 @@ router.get("/genre/create", genreController.genre_get_create);
 router.post("/genre/create",genreController.genre_post_create);
 
 //handles deletion of genres
-router.get("/genre/delete", genreController.genre_get_delete);
+router.get("/genre/:id/delete", genreController.genre_get_delete);
 
-router.post("/genre/delete", genreController.genre_post_delete);
+router.post("/genre/:id/delete", genreController.genre_post_delete);
 
 //handles updating genres
-router.get("/genre/update", genreController.genre_get_update)
+router.get("/genre/:id/update", genreController.genre_get_update)
 
-router.post("/genre/update", genreController.genre_post_update)
+router.post("/genre/:id/update", genreController.genre_post_update)
 
 //displays the detail of the selected genre
-router.get("/genre:id", genreController.genre_detail)
+router.get("/genre/:id", genreController.genre_detail)
 
 //displays list of genres
 router.get("/genre", genreController.genre_list)
+
+
+module.exports = router
