e29892a6b41c65ae9936ab9538843611dc7faa84
olivia <olivia.brantley9@gmail.com>
2023-08-01 10:02:22 -0700

    added basic starter

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return(
+        <div>
+            <div>Hello There</div>
+            <button>Click Here for Movie Title</button>
+        </div>
+    )
+}
+
+export default App
