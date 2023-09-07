d40648750cae76fb401ebcfdabd37126aee9287c
olivia <olivia.brantley9@gmail.com>
2023-09-06 20:35:00 -0700

    redo server

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,24 @@
 import React from "react";
+import {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+    const[movies, setMovies] = useState([]);
+
+
+
+        axios.get('http://localhost:3000/'
+        ).then((res) =>{
+            setMovies(res.data)
+        }).catch((err) => console.error(err))
+
+ 
     return(
         <div>
             <div>Hello There</div>
-            <button>Click Here for Movie Title</button>
+            <button >Click Here for Movie Title</button>
+            <div>{movies}</div>
+     
         </div>
     )
 }
